72defdcbb8e7abda44f5fbe86f53b9ae9b8d10e4
Reinier Pamintuan <reinier.pamintuan@gmail.com>
2016-12-13 23:37:49 -0800

    Add files via upload

---
+++ ZombAi.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZombAi : MonoBehaviour {
+	public float walkSpeed = 20.0F;
+	public float jumpSpeed = 20.0F;
+	public float runSpeed = 20.0F;
+	public float gravity = 120.0F;
+	public Transform target;
+	public float speed = 220.0F;
+	public Animator anim;
+	
+	private Vector3 moveDirection = Vector3.zero;
+	private CharacterController controller;
+	private Collider col;
+	
+	void Start()
+	{
+		controller = GetComponent<CharacterController>();
+		col = GetComponent<CapsuleCollider>();
+		anim = GetComponent<Animator> ();
+	}
+
+
+
+	void Update()
+	{
+
+		    
+			Vector3 targetDir = target.position - transform.position;
+			float step = speed * Time.deltaTime;
+			float distance = Vector3.Distance (target.position, transform.position);
+			targetDir.y = 0;
+			Vector3 newDir = Vector3.RotateTowards (transform.forward, targetDir, step, 0.0F);
+			transform.rotation = Quaternion.LookRotation (newDir);
+			if (distance > 3) {
+			anim.SetFloat("Speed", 0);
+
+			Debug.DrawRay (transform.position, newDir, Color.red);
+			transform.rotation = Quaternion.LookRotation (newDir);
+
+			newDir.y -= gravity * Time.deltaTime;
+			controller.Move (newDir * Time.deltaTime*2.0F);
+
+		} else {
+			anim.SetFloat("Speed", 1);
+		}
+	
+	}
+	void HitByRay(){
+		anim.SetFloat ("Dead", 2);
+	}
+}
---
+++ rayCastShot.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rayCastShot : MonoBehaviour {
+
+	LineRenderer line1;
+	public float RayCastHitZ = 5.0f;
+
+	void Start(){
+		line1 = gameObject.GetComponent<LineRenderer>();
+		line1.enabled = false;
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.GetButtonDown ("Fire1")) {
+			StopCoroutine("FireLaser");
+			StartCoroutine("FireLaser");
+
+
+		}
+
+	}
+
+	IEnumerator FireLaser(){
+		line1.enabled = true;
+
+		while (Input.GetButtonDown ("Fire1")) {
+			Ray ray = new Ray(transform.position, transform.right*-1);
+			RaycastHit hit;
+
+	//		if(Physics.Raycast(ray, out hit, 100))
+	//		{
+				line1.SetPosition (0, ray.origin);
+	//		}
+	//		else
+	//		{
+				line1.SetPosition (1, ray.GetPoint(100));
+	//		}
+			if(Physics.Raycast(ray, out hit, 100)){
+				hit.transform.SendMessage("HitByRay");
+			}
+			yield return null;
+		}
+
+		line1.enabled = false;
+	}
+}
